4645c477383897592e0776e1f0d25c0ce0ad2232
Connor Vickers <connor@pal-nat186-74-79.itap.purdue.edu>
2015-10-17 23:51:25 -0400

    changed from Julie

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -30,6 +30,40 @@ class UserController < ApplicationController
   		render json: ret.to_json
   	end
 
+  	def get2
+  		#if not user make and then save
+  		
+  		user = User.find_by(number: params[:number])
+  		if (user == nil)
+  			user = User.new(user_params)
+  			user.save();
+  		else
+  			user.update(user_params)
+  		end
+
+  		if (params[:friends] != nil)
+  			friends = JSON.parse(params[:friends])
+  		else
+  			friends = []
+  		end
+
+  		if (friends.length > 0)
+  			shortestFriend = User.find_by(number: friends[0].to_i)
+	  		shortestDist = get_dist(user,shortestFriend)
+	  		for friendnumber in friends do
+	  			friend = User.find_by(number: friendnumber.to_i)
+	  			if (friend != nil && get_dist(user,friend) < shortestDist)
+					shortestFriend = friend
+				end
+	  		end
+	  		#todo if angle less than certain distance
+	  		angle = get_dir(user,shortestFriend) * (180/(2*3.14159))
+	  	else
+	  		angle = -1
+	  	end
+  		render plain: "[" + "degrees:" + angle.to_s + "]"
+  	end
+
   	private
   		def user_params
     		params.permit(:number, :long, :lat)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'user#index'
   get '/api', to: 'user#get'
+  get '/api2', to: 'user#get2'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
