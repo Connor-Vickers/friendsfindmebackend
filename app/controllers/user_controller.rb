8fd1bbc6969ca25fcf1c591cbaf7af464d90f452
Connor Vickers <connor@pal-nat186-74-79.itap.purdue.edu>
2015-10-18 08:53:02 -0400

    customized for julie

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -55,7 +55,7 @@ class UserController < ApplicationController
   		end
 
   		def user_params
-    		params.permit(:number, :long, :lat)
+    		params.require(:number, :long, :lat)
   		end
 
   		def get_dir(you,them)
